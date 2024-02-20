4d2f9205629bde0b024c3680e7678ee1c77fde96
informaticscorporation <corporationinformatics@gmail.com>
2024-02-20 11:16:44 +0100

    ciao

---
+++ gianfra/script.js
@@ -0,0 +1,55 @@
+//constante e variabili
+const nome = "marco"
+
+let cognome = "montalto"
+
+// operatori
+ // + ,- ,/ ,* ,%
+nome + cognome;
+
+nome - cognome
+
+nome * cognome
+
+nome / cognome
+
+nome % cognome === 0 
+
+// operatori di confronto
+  // >, <, >=,  <=
+
+  nome < cognome;
+
+  nome > cognome
+  
+  nome >= cognome
+  
+  nome <= cognome
+  
+//operatori logici
+  // &&, || , !, ??   
+  nome && cognome;
+
+  nome || cognome
+  
+  nome != cognome
+  
+  nome ?? cognome
+
+//condizionale
+
+if(nome === cognome) {
+
+ 
+
+} else {
+
+
+}
+
+// function
+
+function somma() {
+  nome + cognome;
+
+}
